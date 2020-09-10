57dfb4e320e958d0c374b7bac0bde36dd0d7a878
Administrator <admin@example.com>
2020-09-09 21:38:03 -0400

    add additional env variables, rename current

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -5,37 +5,53 @@ function log() {
 }
 
 # Check for env variables
-if [ -z "$repoRemote" ]
+if [ -z "$REPO_REMOTE" ]
 then
-    log "FAIL: Repo URL not specified (repoRemote)"
+    log "FAIL: Repo URL not specified (REPO_REMOTE)"
     exit
 fi
 
-if [ -z "$repoBranch" ]
+if [ -z "$REPO_BRANCH" ]
 then
-    log "FAIL: Repo branch not specified (repoBranch)"
+    log "FAIL: Repo branch not specified (REPO_BRANCH)"
     exit
 fi
 
-if [ -f "$localPath" ]
+if [ -z "$REPO_DOMAIN" ]
 then
-    log "FAIL: Local path not present (localPath)"
+    log "FAIL: Repo branch not specified (REPO_DOMAIN)"
     exit
 fi
 
-if [ -z "$pushWait" ]
+if [ -f "$LOCAL_PATH" ]
 then
-    export pushWait=2
+    export LOCAL_PATH='/repo'
+fi
+
+if [ -z "$PUSH_WAIT" ]
+then
+    export PUSH_WAIT=2
+fi
+
+if [ -z "$GIT_EMAIL" ]
+then
+    export GIT_EMAIL=gitwatch@localhost
+fi
+
+if [ -z "$GIT_USER" ]
+then
+    export GIT_USER=gitwatch
+fi
 
 log "Adding ssh key"
 eval `ssh-agent`
-ssh-add <(echo "$privateKey")
+ssh-add <(echo "$PRIVATE_KEY")
 
 log "Adding ssh host key"
 mkdir ~/.ssh
-ssh-keyscan -t rsa gitlab.com >> ~/.ssh/known_hosts
+ssh-keyscan -t rsa $REPO_DOMAIN >> ~/.ssh/known_hosts
 
 log "Starting git-watch"
-git config --global user.email "gitwatch@localhost"
-git config --global user.name "Gitwatch"
-/gitwatch.sh -r $repoRemote -b $repoBranch -s $pushWait $localPath
+git config --global user.email $GIT_EMAIL
+git config --global user.name $GIT_USER
+/gitwatch.sh -r $REPO_REMOTE -b $REPO_BRANCH -s $PUSH_WAIT $LOCAL_PATH
